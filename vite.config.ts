6c614bbbf5bc922d70f77caf0be88b49c372c2f2
kazoottt <turbopm@outlook.com>
2024-03-13 16:43:05 +0800

    Add vite-plugin-libcss to package.json and pnpm-lock.yaml

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
+import libCss from "vite-plugin-libcss";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), libCss()],
   build: {
     lib: {
       // 入口文件将包含可以由你的包的用户导入的导出：
